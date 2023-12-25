7fa56ef6c3eb85a0ea5dcb54c5dcaf1bfb035fd5
Izaias Lima <izaiaslima356@gmail.com>
2023-12-25 11:05:36 -0300

    class: how useState works

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { PostType } from "../@types/post";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
@@ -15,6 +16,14 @@ export function Post({ author, content, publishedAt }: Omit<PostType, "id">) {
 		locale: ptBR,
 		addSuffix: true,
 	});
+
+	const [comments, setComments] = useState([1, 2, 3]);
+
+	function hadleCreateNewComment(event: React.FormEvent) {
+		event.preventDefault();
+
+		setComments([...comments, comments.length + 1]);
+	}
 
 	return (
 		<article
@@ -66,15 +75,18 @@ export function Post({ author, content, publishedAt }: Omit<PostType, "id">) {
 			<form
 				className="commentForm w-full mt-[1.5rem] pt-[1.5rem] border-t
 			border-gray-600 transition-all"
+				onSubmit={hadleCreateNewComment}
 			>
 				<strong className="leading-relaxed  text-white">
 					Deixe seu feedback
 				</strong>
+
 				<textarea
 					placeholder="Deixe seu comentário"
 					className="w-full bg-gray-900 border-0 resize-none p-4 rounded-lg
 					h-[6rem] leading-snug mt-4 "
 				></textarea>
+
 				<div className="invisible max-h-0">
 					<button
 						type="submit"
@@ -88,9 +100,9 @@ export function Post({ author, content, publishedAt }: Omit<PostType, "id">) {
 			</form>
 
 			<div className="mt-5">
-				<Comment />
-				<Comment />
-				<Comment />
+				{comments.map((comment) => (
+					<Comment key={comment} />
+				))}
 			</div>
 		</article>
 	);
